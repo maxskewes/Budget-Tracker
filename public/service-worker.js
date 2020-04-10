8c3de4673a4ac6305f16369dc70ec04d56a05dad
Max <maxskewes@gmail.com>
2020-04-09 21:50:09 -0700

    almost there

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,13 @@
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
+    '/index.js',
     '/manifest.webmanifest',
-    '/style.css',
-    '/assets/js/loadImages.js',
-]
+    '/styles.css'
+];
 
 
-const CACHE_NAME = "static-cache-v5";
+const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener("install", function(evt) {
@@ -19,3 +19,45 @@ self.addEventListener("install", function(evt) {
     );
     self.skipWaiting();
   });
+
+  self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+  
+  // fetch
+  self.addEventListener("fetch", function(evt) {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+  }});
+  
